de3995583f5cd8e2b431f3e5405517ced25cf45d
Andrea <andrea.mazzini00@gmail.com>
2023-01-28 12:55:06 +0100

    changed db

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-DATABASE_URL = "postgresql+psycopg2://postgres:postgres@db:5432/postgres"
+DATABASE_URL = "postgres://myuser:ssmFLhZpOPcf2NEyvKUrcb1jHhoPuXHi@dpg-cfagn402i3mjduikq6n0-a.frankfurt-postgres.render.com/mydb_yj6u" #"postgresql+psycopg2://postgres:postgres@db:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
