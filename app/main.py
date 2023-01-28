38d504f8e07f07dfe9169b8ce07e68784cfeb9d0
Andrea <andrea.mazzini00@gmail.com>
2023-01-28 16:09:32 +0100

    changed start

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routers import abbonamenti,auth,transazioni,users,utils
+import uvicorn
 
 app=FastAPI()
 
@@ -8,3 +9,7 @@ app.include_router(auth)
 app.include_router(transazioni)
 app.include_router(users)
 app.include_router(utils)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=80, log_level="info",reload=True)
